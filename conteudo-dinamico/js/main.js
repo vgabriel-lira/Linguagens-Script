51bcbb6376c8e766a0e400f565e8b42d477f7da2
vgabriel-lira <vgabriel.lira@gmail.com>
2026-01-29 00:37:29 -0300

    exercício bandeiras

--- conteudo-dinamico/js/main.js
+++ conteudo-dinamico/js/main.js
@@ -2,15 +2,19 @@ import flags from './model/flags.js';
 
 const main = document.querySelector('main');
 
-for (let i = 0; i < 30 && i < flags.length; i++) {
-  main.insertAdjacentHTML('beforeend', createFlagView(flags[i]));
-}
+flags.forEach(flag => {
+  const div = document.createElement('div');
+  div.className = 'flag col-2 my-2 text-center';
 
-function createFlagView(flag) {
-  return `
-    <div class="flag col-2 my-2 text-center">
-      <img src="${flag.image}" alt="${flag.name}" class="img-fluid border">
-      <p>${flag.name}</p>
-    </div>
-  `;
-}
+  const img = document.createElement('img');
+  img.src = flag.image;
+  img.alt = flag.name;
+
+  const p = document.createElement('p');
+  p.textContent = flag.name;
+
+  div.appendChild(img);
+  div.appendChild(p);
+
+  main.appendChild(div);
+});
